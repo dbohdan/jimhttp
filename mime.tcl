0c6386e6a1e653ce6796e8720a7fd30d16c0e093
Danyil Bohdan <danyil.bohdan@gmail.com>
2015-06-01 14:27:15 +0300

    mime.tcl: Detect MIME type by filename

--- mime.tcl
+++ mime.tcl
@@ -1,38 +1,76 @@
 # MIME type detection by filename extension.
 # Copyright (C) 2014, 2015 Danyil Bohdan.
 # License: MIT
-namespace eval ::mime {}
 
-set ::mime::byExtension {
-    .c      text/plain
-    .conf   text/plain
-    .css    text/css
-    .csv    text/csv
-    .gif    image/gif
-    .gz     application/gzip
-    .h      text/plain
-    .htm    text/html
-    .html   text/html
-    .jpg    image/jpeg
-    .jpeg   image/jpeg
-    .js     application/javascript
-    .json   application/json
-    .pdf    application/pdf
-    .png    image/png
-    .ps     application/postscript
-    .sh     text/plain
-    .tcl    text/plain
-    .txt    text/plain
-    .xhtml  application/xhtml+xml
-    .xml    application/xml
-    .zip    application/zip
+namespace eval ::mime {
+    variable version 1.2.0
+
+    variable mimeDataInverted {
+        text/plain {
+            makefile
+            COPYING
+            LICENSE
+            README
+            Makefile
+            .c
+            .conf
+            .h
+            .log
+            .md
+            .sh
+            .tcl
+            .terms
+            .tm
+            .txt
+            .wiki
+            .LICENSE
+            .README
+        }
+        text/css                .css
+        text/csv                .csv
+        image/gif               .gif
+        application/gzip        .gz
+        text/html {
+            .htm
+            .html
+        }
+        image/jpeg {
+            .jpg
+            .jpeg
+        }
+        application/javascript  .js
+        application/json        .json
+        application/pdf         .pdf
+        image/png               .png
+        application/postscript  .ps
+        application/xhtml       .xhtml
+        application/xml         .xml
+        application/zip         .zip
+    }
+
+    variable byFilename {}
+    variable byExtension {}
+    foreach {mimeType files} $mimeDataInverted {
+        foreach file $files {
+            if {[string index $file 0] eq "."} {
+                lappend byExtension $file $mimeType
+            } else {
+                lappend byFilename $file $mimeType
+            }
+        }
+    }
+    unset mimeDataInverted
 }
 
 proc ::mime::type {filename} {
-    global ::mime::byExtension
+    variable byFilename
+    variable byExtension
+    set tail [file tail $filename]
     set ext [file extension $filename]
-    if {[dict exists $::mime::byExtension $ext]} {
-        return [dict get $::mime::byExtension $ext]
+    if {[dict exists $byFilename $tail]} {
+        return [dict get $byFilename $tail]
+    } elseif {[dict exists $byExtension $ext]} {
+        return [dict get $byExtension $ext]
     } else {
         return application/octet-stream
     }
