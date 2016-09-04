750967c072c78e79085b85f3137e0f405fc022c1
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-09-04 20:39:36 +0300

    vagrant/install-jimtcl.sh: Install Jim Tcl 0.77
    This affects Travis CI, too.

--- vagrant/install-jimtcl.sh
+++ vagrant/install-jimtcl.sh
@@ -5,7 +5,7 @@ apt-get -y install git build-essential libsqlite3-dev
 
 git clone https://github.com/msteveb/jimtcl.git
 cd jimtcl
-git checkout --detach 0.76
+git checkout --detach 0.77
 ./configure --with-ext="oo tree binary sqlite3" --enable-utf8 --ipv6 --disable-docs
 make
 make install
