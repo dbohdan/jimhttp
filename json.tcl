1c5cf770d67d6d7c510dc9982e715c039b6d99b6
dbohdan <dbohdan@dbohdan.com>
2017-04-28 11:30:11 +0300

    json.tcl: Update copyright

--- json.tcl
+++ json.tcl
@@ -1,5 +1,5 @@
 # JSON parser / encoder.
-# Copyright (C) 2014, 2015, 2016 dbohdan.
+# Copyright (C) 2014, 2015, 2016, 2017 dbohdan.
 # License: MIT
 
 ### The public API: will remain backwards compatible for a major release
