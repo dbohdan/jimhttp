bcc5f2a2265849ccf0a5cb046be04259fb92b10d
dbohdan <dbohdan@dbohdan.com>
2017-07-28 17:40:58 +0300

    chore(vagrant): Exit if cd fails in shell script

--- vagrant/install-jimtcl.sh
+++ vagrant/install-jimtcl.sh
@@ -4,9 +4,9 @@ apt-get update
 apt-get -y install git build-essential libsqlite3-dev
 
 git clone https://github.com/msteveb/jimtcl.git
-cd jimtcl
+cd jimtcl || exit 1
 git checkout --detach 0.77
 ./configure --with-ext="oo tree binary sqlite3" --enable-utf8 --ipv6 --disable-docs
 make
 make install
-cd ..
+cd .. || exit 1
--- vagrant/test-server.sh
+++ vagrant/test-server.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Run jimhttp in a loop. curl localhost:8080/quit to restart.
-cd /jimhttp
+cd /jimhttp || exit 1
 echo Starting example.tcl on port 8080.
 while true; do
     jimsh example.tcl -i 0.0.0.0 -p 8080 -v 5
