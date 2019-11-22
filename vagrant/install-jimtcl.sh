c5d3cb44f797f59798957b2a505a1796c32c1d15
dbohdan <dbohdan@dbohdan.com>
2019-11-22 19:18:22 +0000

    chore(install-jimtcl): build version 0.79

--- vagrant/install-jimtcl.sh
+++ vagrant/install-jimtcl.sh
@@ -5,8 +5,8 @@ apt-get -y install git build-essential libsqlite3-dev
 
 git clone https://github.com/msteveb/jimtcl.git
 cd jimtcl || exit 1
-git checkout --detach 0.77
-./configure --with-ext="oo tree binary sqlite3" --enable-utf8 --ipv6 --disable-docs
+git checkout --detach 0.79
+./configure --with-ext="oo tree binary sqlite3" --utf8 --ipv6 --disable-docs
 make
 make install
 cd .. || exit 1
